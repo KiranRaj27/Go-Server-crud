323ee6fa5c71056416260812c3606c7acd4ee111
Kiranraj27 <kiranraj586@gmail.com>
2023-08-17 08:15:20 +0530

    Delete api

--- main.go
+++ main.go
@@ -27,6 +27,30 @@ func getMovies(w http.ResponseWriter,r *http.Request){
 	json.NewEncoder(w).Encode(movies)
 }
 
+func deleteMovie(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type","application/json")
+	params := mux.Vars(r)
+
+	for index, item := range movies {
+		if item.ID == params["id"]{
+			movies = append(movies[:index],movies[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie(w http.ResponseWriter,r *http.Request){
+	w.Header().Set("Content-Type","application/json")
+	params := mux.Vars(r)
+
+	for _,item := range movies {
+		if item.ID == params["id"]{
+			json.NewEncoder(w).Encode(item)
+		}
+	}
+}
+
 func main(){
 	r := mux.NewRouter()
 
@@ -34,10 +58,10 @@ func main(){
 	movies=append(movies,Movie{ID:"2",Isbn:"3348",Title:"Movie Two",Director:&Director{Firstname:"Steve",Lastname:"Smith"}})
 
 	r.HandleFunc("/movies",getMovies).Methods("GET")
-	// r.HandleFunc("/movie/{id}",getMovie).Methods("GET")
+	r.HandleFunc("/movie/{id}",getMovie).Methods("GET")
 	// r.HandleFunc("/movie",createMovie).Methods("POST")
 	// r.HandleFunc("/movie/{id}",updateMovie).Methods("PUT")
-	// r.HandleFunc("/movie/{id}",deleteMovie).Methods("DELETE")
+	r.HandleFunc("/movie/{id}",deleteMovie).Methods("DELETE")
 
 	fmt.Printf("Starting server at port 8000\n")
 	log.Fatal(http.ListenAndServe(":8000",r))
